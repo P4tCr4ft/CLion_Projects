8bdaec5b17ca3e69e1478b86a78ddb1e0e0d7865
dswain <david.swain@dsto.defence.gov.au>
2018-08-20 12:48:30 +0930

    20 August 2018. Adding notes

--- C_Projects/Udemy_Sec12PointerArithmetic_CLion/main.c
+++ C_Projects/Udemy_Sec12PointerArithmetic_CLion/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stddef.h>
+
+int arraySum(int array[], const int n)
+{
+    int sum = 0;
+    int * ptr;
+    int * const arrayEnd = array + n;// constant pointer, so address cannot change
+
+    for (ptr = array; ptr < arrayEnd; ++ptr)// ptr = array, same as ptr = &array[0], arrayEnd is last element address
+                                            // ptr + 1 just goes to the next address
+
+
+
+        sum += *ptr;// dereference pointer, pointing to each address in array, as it iterates
+
+    return sum;
+}
 
 int main() {
+
+//    int arraySum(int array[], const int n); // Not sure why is prototyping arraySum function here, in the example?
+
+    int values[10] = {3, 7, -9, 3, 6, -1, 7, 9, 1, -5};
+
+    printf("The sum is %i\n", arraySum(values, 10));
+
 
     return 0;
 }
