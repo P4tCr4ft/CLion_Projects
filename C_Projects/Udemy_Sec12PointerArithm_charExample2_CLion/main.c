a399afde72dce6cb9e7262ec85267c82bcbc1281
STEVESPRO3\Steve <patrickcarnivore@gmail.com>
2018-08-27 10:41:42 +0930

    27 Aug 2018. char example continuing

--- C_Projects/Udemy_Sec12PointerArithm_charExample2_CLion/main.c
+++ C_Projects/Udemy_Sec12PointerArithm_charExample2_CLion/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    char multiple[] = "a string";
+
+
+    printf("some is %s\n", multiple);
+    printf("size of some is %i\n", sizeof(multiple));
+    printf("strlen of multiple is %i\n", strlen(multiple));
+
     return 0;
 }
